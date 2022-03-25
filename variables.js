cbd1ca54b6e2b0353c42c5d707049567353cfe68
Emmaline <emmalinerose19@gmail.com>
2022-03-25 11:47:56 -0500

    Completion 4/5

--- variables.js
+++ variables.js
@@ -1,18 +1,15 @@
-//////////////////PROBLEM 1////////////////////
-/* Create a variable called 'myName' with a value that is your name as a string. */
-
 //CODE HERE 
-
+var myname = `Emmaline`
 //////////////////PROBLEM 2////////////////////
 /* Create a variable called 'faveNum' with a value that is your favorite number. */
 
 //CODE HERE
-
+var faveNum = 9
 //////////////////PROBLEM 3////////////////////
 /* Create a variable called 'lovesCode' and assign it a boolean value. */
 
 //CODE HERE
-
+var lovesCode = Boolean(true)
 //////////////////PROBLEM 4////////////////////
 /*
   Write an if-statement that checks if the faveNum variable is 
@@ -21,6 +18,10 @@
 */
 
 //CODE HERE
+if(faveNum == 13){
+  console.log(`lucky 13!`)
+}else(console.log(`not lucky 13`))
+//////////////////PROBLEM 5////////////
 
 //////////////////PROBLEM 5////////////////////
 /*
@@ -28,3 +29,4 @@
 */
 
 //CODE HERE
+for(i = faveNum, i )
