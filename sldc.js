b818b6d4c6ab7981d5d414885070094a486420e5
Emmaline <emmalinerose19@gmail.com>
2022-03-25 11:22:14 -0500

    completion

---
+++ sldc.js
@@ -0,0 +1,31 @@
+
+
+
+// In this section you will answer some work through problems related 
+//to the Software Development Lifecycle
+
+//////////////////PROBLEM 1////////////////////
+/*
+    Reorganize the following steps of the Software Development Lifecycle 
+    to be in their correct order.
+*/
+
+// Planning
+// Analysis of Requirements
+// Design
+// Implementation
+// Testing & Integration
+// Maintain
+
+//////////////////PROBLEM 2////////////////////
+/*
+    Create a variable for each of the above steps of the Software Development 
+    Lifecycle and set the values of each variable to a definition of the step.
+    The first one is done for you. 
+*/
+const planning = "This is the first step, in which the end goal of the software is determined and defined. Developers typically will see what systems are already in place, and determine what features will need to be added." 
+const analysisOfRequirements = "This is the second step, the end goal being a detailed breakdown of requirements on a grand scale such as costs, resources, design, timeframe etc."
+const design = "Third, we have what is essentially a blueprint of what the team is going to build together, including things like hardware requirements, etc."
+const implementation = "Fourth, coding can finally begin. The team knows what needs to be done and how, its just a matter of putting in the man hours and being flexible with snags you may run into to meet the end goal of a working prototype"
+const testingAndIntegration = "Fifth, we're entering bug territory. Lots of repeatedly putting the prototype under pressure to perform in every scenario to address any issues and fix them until stability is reached, prompting installation with the customer"
+const maintain = "Whoa, whoa, whoa, don't even think about walking away yet, this product is a like a baby, it will need constant support and a village to squash any bugs that will 100% pop up at some point. The developer is in constant communication with the customer to ensure the product is running properly"
